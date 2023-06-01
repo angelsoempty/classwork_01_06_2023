6398f14ddedb6864655ea26b8f6e4eee27b7df50
angelsoempty <denatamanov1@gmail.com>
2023-06-01 19:16:02 +0300

    2

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+a = int(input('Введіть перше число: '))
+b = int(input('Введіть друге число: '))
+
+sum = 0
+for i in range(a, b + 1):
+    sum += i
+print(f'Сума чисел діапазону {sum}')
