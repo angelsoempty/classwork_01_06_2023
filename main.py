07c285c962d576ce739e0452bfdbe5434ac073fc
angelsoempty <denatamanov1@gmail.com>
2023-06-01 19:34:14 +0300

    9

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def filter_capitalized_strings(strings):
-    capitalized_strings = [string for string in strings if string[0].isupper()]
-    return capitalized_strings
+def filter_python_strings(strings):
+    python_strings = [string for string in strings if "Python" in string]
+    return python_strings
+input_strings = input('Введіть рядки: ').split()
+filtered_strings = filter_python_strings(input_strings)
 
-input_strings = input("Введіть рядки: ").split()
-filtered_strings = filter_capitalized_strings(input_strings)
-
-print("Список рядків що починаються з великої літери: ")
+print('Список рядків що містять слово Python: ')
 for string in filtered_strings:
     print(string)
