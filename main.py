c3063543852fc7638cb9765135fe0f8a42d16677
angelsoempty <denatamanov1@gmail.com>
2023-06-01 19:37:16 +0300

    11

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def get_info(self):
-        print("Ім'я:", self.name)
-        print("Вік:", self.age)
+class BankAccount:
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+        print("Гроші успішно додано. Новий баланс:", self.balance)
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print("Гроші успішно знято. Новий баланс:", self.balance)
+        else:
+            print("Недостатньо коштів на рахунку.")
 
-student1 = Student("Gleb", 17)
-student1.get_info()
+account1 = BankAccount("1234567890", 1000)
+account1.deposit(100)
+account1.withdraw(400)
