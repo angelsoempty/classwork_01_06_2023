480a479fd2a9f3d79043217a2774c3771596ee62
angelsoempty <denatamanov1@gmail.com>
2023-06-01 19:38:59 +0300

    12

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-class BankAccount:
-    def __init__(self, account_number, balance=0):
-        self.account_number = account_number
-        self.balance = balance
-    def deposit(self, amount):
-        self.balance += amount
-        print("Гроші успішно додано. Новий баланс:", self.balance)
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-            print("Гроші успішно знято. Новий баланс:", self.balance)
-        else:
-            print("Недостатньо коштів на рахунку.")
-
-account1 = BankAccount("1234567890", 1000)
-account1.deposit(100)
-account1.withdraw(400)
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_info(self):
+        print("Назва книги: ", self.title)
+        print("Автор: ", self.author)
+book1 = Book("Багатий тато Бідний тато", "Роберт Кіосакі")
+book1.get_info()
