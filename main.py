b7e7a98bda10f9f00c589577c9b00593a614b63f
angelsoempty <denatamanov1@gmail.com>
2023-06-01 19:32:35 +0300

    8

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-students = {}
-n = int(input("Введіть кількість студентів: "))
-for _ in range(n):
-    name = input("Введіть ім'я студента: ")
-    grade = float(input("Введіть оцінку з математики: "))
-    students[name] = grade
-max_grade = max(students.values())
-top_student = [name for name, grade in students.items() if grade == max_grade]
-print("Студент(и) з найвищою оцінкою:")
-for student in top_student:
-    print(student)
+def filter_capitalized_strings(strings):
+    capitalized_strings = [string for string in strings if string[0].isupper()]
+    return capitalized_strings
+
+input_strings = input("Введіть рядки: ").split()
+filtered_strings = filter_capitalized_strings(input_strings)
+
+print("Список рядків що починаються з великої літери: ")
+for string in filtered_strings:
+    print(string)
